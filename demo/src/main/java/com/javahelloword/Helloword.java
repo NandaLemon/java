ed9fd5e7c5f9715a03b8ba6a216f7009da3d7910
NandaLemon <rizkynandapr@gmail.com>
2023-08-03 19:31:27 +0700

    java_smst_antara

---
+++ demo/src/main/java/com/javahelloword/Helloword.java
@@ -0,0 +1,23 @@
+package com.javahelloword;
+
+public class Helloword {
+    public static void main(String[] args) {
+        byte iniByte = 12;
+        short iniShort_Konversi = iniByte;
+
+        short iniShort = 32_767;
+        int iniInt = 2_147_483_647;
+        long iniLong = 923_337_203;
+
+        float iniFloat = 10.12F;
+        long iniLong2 = (long) iniFloat;
+
+        double iniDouble = 12.2424;
+
+        int decimalInt = 25;
+        int hexInt = 0xABCDEF;
+        int binInt = 0b1010101010101;
+
+        System.out.println("Hello Word");
+    }
+}
